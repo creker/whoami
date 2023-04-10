5472a1566a04c708e3947f3ca33327cc15fb17b4
Артем Антоненко <sam901@yandex.ru>
2023-04-10 15:21:15 +0300

    Clamp normal distribution to max

--- internal/random/normdist.go
+++ internal/random/normdist.go
@@ -6,5 +6,9 @@ import (
 )
 
 func NormFloat64(min float64, max float64) float64 {
-	return math.Abs(rand.NormFloat64()*((max-min)/3)) + min
+	val := math.Abs(rand.NormFloat64()*((max-min)/3)) + min
+	if val > max {
+		val = max
+	}
+	return val
 }
